870b478621ac11e2ff2aebf1ff7d241d0e062cbd
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-07-30 19:25:26 +0530

    Add files via upload

---
+++ Basics/src/DataType.java
@@ -0,0 +1,13 @@
+
+class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		float pi = (float)3.14; // 1st way
+		
+		float a = 498.897f; // 2nd way
+		
+		String name = "Raj";
+	}
+}
