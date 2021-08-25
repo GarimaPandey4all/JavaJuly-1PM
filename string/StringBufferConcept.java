354d0b56dc47a9bc855ce6db49ccdec5dbc1af27
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-25 20:10:28 +0530

    Add files via upload

---
+++ string/StringBufferConcept.java
@@ -0,0 +1,21 @@
+package com.brainmentors.java.string;
+
+public class StringBufferConcept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StringBuffer sb = new StringBuffer();
+		System.out.println(sb.capacity()+" "+sb.length()); // default capacity is 16
+		
+		sb.append("Hello");
+		System.out.println(sb.capacity()+" "+sb.length());
+		
+		sb.append("How are you I am Fine skabbgudabgiueuirkj j jk jjn eoqnveierbiebvroeri");
+		System.out.println(sb.capacity()+" "+sb.length());
+		
+		sb.append("How are");
+		System.out.println(sb.capacity()+" "+sb.length());
+	}
+
+}
